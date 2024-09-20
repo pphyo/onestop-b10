104e00d4fcec059e41a0321c86361f7205cdc0c8
pphyo <pyaephyo.jdc@gmail.com>
2024-09-20 10:06:47 +0630

    java.util.Map Interface Mapping

--- jakarta-persistence/03-collection-mapping/src/main/java/com/jdc/cm/MediaAsset.java
+++ jakarta-persistence/03-collection-mapping/src/main/java/com/jdc/cm/MediaAsset.java
@@ -2,7 +2,7 @@ package com.jdc.cm;
 
 import java.io.Serializable;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
@@ -12,43 +12,58 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapKeyColumn;
+import jakarta.persistence.MapKeyEnumerated;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
-@Entity @Table(name = "media_asset_tbl")
+@Getter
+@Setter
+@Entity
+@Table(name = "media_asset_tbl")
 public class MediaAsset implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private Integer id;
 	private String name;
 	private String format;
-	
+
 	@ElementCollection
-	@CollectionTable(name = "permission_tbl", 
-		joinColumns = @JoinColumn(name = "meida_asset_id"))
+	@CollectionTable(name = "permission_tbl", joinColumns = @JoinColumn(name = "meida_asset_id"))
 	@Column(name = "permission", nullable = false)
 	@Enumerated(EnumType.STRING)
 	private List<Permission> permissions;
-	
+
+//	@ElementCollection
+//	@Column(nullable = false)
+//	private Deque<LocalDateTime> modifications;
+//	
+//	@ElementCollection
+//	@Column(name = "permission_number", nullable = false)
+//	private Set<Integer> permissionNumbers;
+//	
+//	@ElementCollection
+//	@CollectionTable(name = "media_asset_tag", 
+//		joinColumns = @JoinColumn(name = "media_asset_id"))
+//	@MapKeyColumn(name = "tag_key")
+//	@Column(name = "tag", nullable = false)
+//	private Map<Integer, String> tags;
+
 	@ElementCollection
-	@Column(name = "permission_number", nullable = false)
-	private Set<Integer> permissionNumbers;
-	
+	@MapKeyColumn(name = "permission_map_key")
+	@MapKeyEnumerated(EnumType.STRING)
+	@Enumerated(EnumType.STRING)
+	private Map<Permission, PermissionNumber> permssionMap;
+
 	public enum Permission {
-		READ, WRITE, EXECUTABLE
+		EXECUTABLE, WRITE, READ
+	}
+
+	public enum PermissionNumber {
+		ONE, TWO, FOUR
 	}
 
 }
-
-
-
-
-
-
-
-
-
